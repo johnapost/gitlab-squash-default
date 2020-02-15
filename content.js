120fb92d7c7c31b5d7028854880d577fb9a53960
John Apostol <notimetobeidle@gmail.com>
2020-02-14 18:21:36 -0600

    Check delete source branch by default and extend functionality to new and edit MR screens

--- content.js
+++ content.js
@@ -1,9 +1,31 @@
-window.addEventListener('load', () => {
-  checkSquash = setInterval(() => {
-    const checkbox = document.querySelector("input[name='squash']")
+const sourceInputs = [
+  // Checkbox on new and edit MR screen
+  "input#merge_request_force_remove_source_branch",
+
+  // Checkbox on view MR screen
+  "input#remove-source-branch-input"
+]
+
+const squashInputs = [
+  // Checkbox on new and edit MR screen
+  "input#merge_request_squash",
+
+  // Checkbox on view MR screen
+  "input[name='squash']"
+]
+
+// Check the boxes, end interval
+const check = (inputs, interval) => {
+  inputs.forEach(input => {
+    const checkbox = document.querySelector(input)
     if (checkbox) {
       checkbox.checked = true;
-      clearInterval(checkSquash);
+      clearInterval(interval);
     }
-  }, 250);
+  })
+}
+
+window.addEventListener('load', () => {
+  const checkSquash = setInterval(() => check(squashInputs, checkSquash), 250);
+  const checkSource = setInterval(() => check(sourceInputs, checkSource), 250);
 });
